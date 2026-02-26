50e618d27dba1c332c69bc13d3c222c8e5c6e1cd
Juan José Hernández Alonso <juanjo.hernandez@movicoders.com>
2026-02-26 21:53:47 +0100

    Start activity explicit intent.

--- app/src/main/java/es/usj/jjhernandez/mobileapps/MainActivity.kt
+++ app/src/main/java/es/usj/jjhernandez/mobileapps/MainActivity.kt
@@ -3,8 +3,6 @@ package es.usj.jjhernandez.mobileapps
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import es.usj.jjhernandez.mobileapps.databinding.ActivityMainBinding
 
@@ -14,23 +12,13 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
-    private val contract = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        result: ActivityResult ->
-        if(result.resultCode == RESULT_OK) {
-            val data = result.data
-            if(data != null) {
-                view.tvResult.text = data.getStringExtra("result")
-            }
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(view.root)
         view.btnStartActivity.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
-            contract.launch(intent)
+            startActivity(intent)
         }
     }
 }
