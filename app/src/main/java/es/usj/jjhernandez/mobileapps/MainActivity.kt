3715c61c5f8a85c9acf40fb4cebae415359ec803
Juan José Hernández Alonso <juanjo.hernandez@movicoders.com>
2026-02-26 21:41:13 +0100

    Added lifecycle logs.

--- app/src/main/java/es/usj/jjhernandez/mobileapps/MainActivity.kt
+++ app/src/main/java/es/usj/jjhernandez/mobileapps/MainActivity.kt
@@ -1,7 +1,7 @@
 package es.usj.jjhernandez.mobileapps
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,8 +9,16 @@ import es.usj.jjhernandez.mobileapps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val LIFECYCLE = "LIFECYCLE"
+    }
+
     private val view by lazy {
         ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private fun log(methodName: String) {
+        Log.v(LIFECYCLE, methodName)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +26,7 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(view.root)
         view.btnSum.setOnClickListener {
-            //view.tvResult.text = sum().toString()
-            navigate()
+            view.tvResult.text = sum().toString()
         }
         view.btnSubtract.setOnClickListener {
             view.tvResult.text = subtract().toString()
@@ -30,6 +37,38 @@ class MainActivity : AppCompatActivity() {
         view.btnDivide.setOnClickListener {
             view.tvResult.text = divide().toString()
         }
+
+        log("onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        log("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        log("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        log("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        log("onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        log("onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
     }
 
     fun sum() : Double {
@@ -57,10 +96,6 @@ class MainActivity : AppCompatActivity() {
         return firstNumber - secondNumber
     }
 
-    fun navigate() {
-        val intent = Intent(this, SecondActivity::class.java)
-        startActivity(intent)
-    }
 }
 
 fun EditText.toDouble(): Double {
