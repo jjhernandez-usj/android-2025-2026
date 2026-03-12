c63ce6643f57a2e94f8139d0db65bfdb522670a4
Juan José Hernández Alonso <juanjo.hernandez@movicoders.com>
2026-03-12 14:47:00 +0100

    Update gradle and cleaning unused imports.

--- app/src/main/java/es/usj/jjhernandez/mobileapps/MainActivity.kt
+++ app/src/main/java/es/usj/jjhernandez/mobileapps/MainActivity.kt
@@ -1,7 +1,6 @@
 package es.usj.jjhernandez.mobileapps
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
