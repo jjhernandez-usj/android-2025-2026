88a2bcc5456d7968581bbefeefc3441c72e52f49
Juan José Hernández Alonso <juanjo.hernandez@movicoders.com>
2026-04-16 09:31:45 +0200

    Implement CustomArrayViewHolderAdapter with ViewHolder pattern

--- app/src/main/java/es/usj/jjhernandez/mobileapps/MainActivity.kt
+++ app/src/main/java/es/usj/jjhernandez/mobileapps/MainActivity.kt
@@ -3,7 +3,7 @@ package es.usj.jjhernandez.mobileapps
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import es.usj.jjhernandez.mobileapps.adapter.CustomArrayAdapter
+import es.usj.jjhernandez.mobileapps.adapter.CustomArrayViewHolderAdapter
 import es.usj.jjhernandez.mobileapps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,14 +18,12 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var items : MutableList<String>
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(views.root)
         items = generate(1000)
         val adapter =
-            CustomArrayAdapter(this, R.layout.row_element, items.toTypedArray())
+            CustomArrayViewHolderAdapter(this, R.layout.row_element, items.toTypedArray())
         views.lvArrayAdapter.adapter = adapter
         views.lvArrayAdapter.setOnItemClickListener { _, _, position, _ ->
             Toast.makeText(this, adapter.getItem(position), Toast.LENGTH_SHORT).show()
